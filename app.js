00bf283e27885f696c20054776e925f27ab7b43b
Robert Safin <robertsafin98@gmail.com>
2023-03-27 19:23:57 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
